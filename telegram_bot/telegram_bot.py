4546cdf379476c09ad775f4a7248e967f1b53f02
shekhar dhruv <dhruvshekhar12@gmail.com>
2022-07-12 00:08:31 +0530

    minor fix

--- telegram_bot/telegram_bot.py
+++ telegram_bot/telegram_bot.py
@@ -1,26 +1,19 @@
-from telegram import InlineKeyboardButton
+from telegram import InlineKeyboardButton, KeyboardButton
 from typing import Union, List
 import telegram_bot
 from _model import *
 import telegram
-from telegram.ext import (
-    Updater,
-    CommandHandler,
-    MessageHandler,
-    Filters,
-    CallbackQueryHandler,
-    PollHandler,
-)
+from telegram.ext import (   Updater,    CommandHandler,    MessageHandler,    Filters,    CallbackQueryHandler,    PollHandler)
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Poll
 import time
 import datetime
-
 import pyfiglet
 import logging
 import logging.config
 import os
 import json
 from dotenv import load_dotenv, find_dotenv
+from time import sleep
 
 load_dotenv(find_dotenv())
 
@@ -32,21 +25,20 @@ data = json_load['questions']
 
 def get_chat_id(update, context):
     chat_id = -1
-
+    
     if update.message is not None:
         chat_id = update.message.chat.id
     elif update.callback_query is not None:
         chat_id = update.callback_query.message.chat.id
     elif update.poll is not None:
         chat_id = context.bot_data[update.poll.id]
-
+        print("CHAT ID ---->",chat_id)
     return chat_id
 
 
 def get_user(update):
     user: User = None
     _from = None
-
     if update.message is not None:
         _from = update.message.from_user
     elif update.callback_query is not None:
@@ -63,9 +55,71 @@ def get_user(update):
 
     return user
 
+# 🎲 Get ready for the quiz 'testquiz'
 
+# this is testing quiz perpose
+
+# 🖊 2 questions
+# ⏱ 10 seconds per question
+# 📰 Votes are visible to the quiz owner
+
+# 🏁 Press the button below when you are ready.
+# Send /stop to stop it.
 def start_command_handler(update, context):
     """Send a message when the command /start is issued."""
+    firstname = update.message.from_user.first_name
+    lastname = update.message.from_user.last_name
+    update.message.reply_text(f'Welcome Dear {firstname}  { lastname} just hit /start command!')
+    startButton = [KeyboardButton("Start the quiz now!!!")]
+    custom_keyboard = [['top-left', 'top-right'],               ['bottom-left', 'bottom-right']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(text="Game start soon!!",reply_markup=reply_markup)
+
+    update.message.reply_text("""
+    🎲 Get ready for the quiz 'testquiz'
+
+this is testing quiz perpose
+
+🖊 2 questions
+⏱ 10 seconds per question
+📰 Votes are visible to the quiz owner
+
+🏁 Press the button below when you are ready.
+Send /stop to stop it.
+    """)
+
+    update.message.reply_text("""
+    🏁 The quiz 'testquiz' has finished!
+
+You answered 1 question:
+
+✅ Correct – 0
+❌ Wrong – 1
+⌛️ Missed – 1
+⏱ 2.9 sec
+
+🥇1st place out of 1. 
+
+You can take this quiz again but it will not change your place on the leaderboard.""")
+
+    # while True:
+    #     play = input("Would you like to start? (Type Y for yes and N for no) ")
+    #     if play.upper() == "Y":
+    #         sleep (1.0)
+    #         update.message.reply_text("Starting in...")
+    #         sleep (1.0)
+    #         update.message.reply_text("3")
+    #         sleep(1.0)
+    #         update.message.reply_text("2")
+    #         sleep(1.0)
+    #         update.message.reply_text("1")
+    #         break
+    # ##3, 2, 1 countdown added wk.4 friday
+    #     elif play.upper() == "N":
+    #         update.message.reply_text (f"Goodbye!")
+    #         return True
+    #     print ("That is not an answer.\n")
+
     add_typing(update, context)
 
     quiz_question = QuizQuestion()
@@ -80,7 +134,6 @@ def start_command_handler(update, context):
 def help_command_handler(update, context):
     """Send a message when the command /help is issued. """
     update.message.reply_text("Type 😄 /start")
-
 
 def new_member(update, context):
     logging.info(f"new_member : {update}")
@@ -110,11 +163,11 @@ def main_handler(update, context):
 
 
 def poll_handler(update, context):
-    logging.info(f"question : {update.poll.question}")
-    logging.info(f"correct option : {update.poll.correct_option_id}")
-    logging.info(f"option #1 : {update.poll.options[0]}")
-    logging.info(f"option #2 : {update.poll.options[1]}")
-    logging.info(f"option #3 : {update.poll.options[2]}")
+    # logging.info(f"question : {update.poll.question}")
+    # logging.info(f"correct option : {update.poll.correct_option_id}")
+    # logging.info(f"option #1 : {update.poll.options[0]}")
+    # logging.info(f"option #2 : {update.poll.options[1]}")
+    # logging.info(f"option #3 : {update.poll.options[2]}")
 
     user_answer = get_answer(update)
     logging.info(f"correct option {is_answer_correct(update)}")
@@ -201,10 +254,14 @@ def is_answer_correct(update):
     counter = 0
 
     for answer in answers:
+        print("answer.voter_count---->",answer.voter_count)
+        print("update.poll.correct_option_id---->",update.poll.correct_option_id)
+        
         if answer.voter_count == 1 and update.poll.correct_option_id == counter:
             ret = True
             break
         counter = counter + 1
+        print("counter---->",counter)
     return ret
 
 
@@ -231,8 +288,7 @@ def main():
     # message handler
     dp.add_handler(MessageHandler(Filters.text, main_handler))
 
-    dp.add_handler(MessageHandler(
-        Filters.status_update.new_chat_members, new_member))
+    dp.add_handler(MessageHandler(      Filters.status_update.new_chat_members, new_member))
 
     # suggested_actions_handler
     dp.add_handler(
@@ -268,8 +324,7 @@ def main():
 
 class DefaultConfig:
     PORT = int(os.environ.get("PORT", 3978))
-    TELEGRAM_TOKEN = os.environ.get(
-        "TELEGRAM_TOKEN", "5146865015:AAG02qIzWeeXib7x1BYDB30D_9-TkhG8qOQ")
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
     MODE = os.environ.get("MODE", "polling")
     WEBHOOK_URL = os.environ.get("WEBHOOK_URL", "")
     LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO").upper()
